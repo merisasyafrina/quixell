7f034e940c73c62a453d0a0adbdf7922dbd056c4
Merisa Syafrina <syafrinamerisa@gmail.com>
2023-09-06 09:44:11 +0700

    about us page

--- resources/views/aboutus/index.blade.php
+++ resources/views/aboutus/index.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
 
-<h4>About Us</h4>
+<div>
+    <h1>ABOUT US</h1>
+    <p class="mt-4">Quixell is one of the biggest international fashion companies, and it belongs to Inditex, one of the world's largest distribution groups.
+        The customer is at the heart of our unique business model, which includes design, production, distribution, and sales, through our extensive retail network.
+        <br><br>
+        Quixell has an ongoing commitment to its customers around the world in providing an excellent customer experience to all. As part of these efforts, we are committed to providing a website that is accessible to the widest possible audience, regardless of technology or ability. Quixell is committed to aligning its website and its operations in substantial conformance with generally-recognized and accepted guidelines and/or standards for website accessibility as these may change from time to time. To assist in these efforts, Quixell has partnered with experienced internationally reputable consultants and is working to increase the accessibility and usability of our website.</p>
+</div>
 
 @endsection
